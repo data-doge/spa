1e7ba78649b1e6b8174822db50db7a9a757946ba
Catt Small <cattsmall@gmail.com>
2020-03-28 17:30:16 -0400

    Define skin color options on screen, make them clickable and run function to set skin color

--- scene-character_select.js
+++ scene-character_select.js
@@ -12,18 +12,34 @@ class CharacterSelect extends Phaser.Scene {
     }
 
     create () {
-        let testText = this.add.text(0,0,'Select a Character!')
+        let testText = this.add.text(400, 60, 'Select a Character!', { fontSize: 40 }).setOrigin(0.5);
+
+        // Check for existing skin color selection
         let skinColor = localStorage.getItem('skinColor')
 
-        if (!skinColor) {
-        localStorage.setItem('skinColor', 'dark')
+        if (skinColor) {
+            // this.continue();
         }
 
-        this.scene.start('Gameplay')
+        // Define skin color options on screen, make them clickable and run function to set skin color
+        let playerOptionDark = this.add.sprite(200, 300, 'player-skin-dark').setInteractive().on('pointerdown', () => { this.setSkinColor('dark') })
+        let playerOptionMedium = this.add.sprite(300, 300, 'player-skin-medium').setInteractive().on('pointerdown', () => { this.setSkinColor('medium') })
+        let playerOptionTan = this.add.sprite(400, 300, 'player-skin-tan').setInteractive().on('pointerdown', () => { this.setSkinColor('tan') })
+        let playerOptionLight = this.add.sprite(500, 300, 'player-skin-light').setInteractive().on('pointerdown', () => { this.setSkinColor('light') })
+        let playerOptionPale = this.add.sprite(600, 300, 'player-skin-pale').setInteractive().on('pointerdown', () => { this.setSkinColor('pale') })
     }
 
     update () {
 
+    }
+
+    setSkinColor (skinColor) {
+        localStorage.setItem('skinColor', skinColor)
+        this.continue()
+    }
+
+    continue () {
+        this.scene.start('Gameplay')
     }
 
 }
