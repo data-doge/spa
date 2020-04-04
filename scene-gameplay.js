f3f2a6dfafb1bb0adc84ddb3ea29766dad462675
Catt Small <cattsmall@gmail.com>
2020-04-04 17:52:02 -0400

    Assign animation and skin color to username

--- scene-gameplay.js
+++ scene-gameplay.js
@@ -19,82 +19,20 @@ class Gameplay extends Phaser.Scene {
         // Tile background
         let floor = this.add.tileSprite(0, 0, 1920, 1080, 'floor-gray').setOrigin(0)
 
-        // Draw player with selected skin color
-        let skinColor = localStorage.getItem('skinColor')
+        // Create group for player character
+        this.mainPlayer = this.add.container(this.cameras.main.centerX, this.cameras.main.centerY)
+        this.physics.world.enable(this.mainPlayer)
 
-        this.player = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'player-skin-' + skinColor)
-        this.player.visible = false
+        // Pull in username and skin color
+        this.mainPlayer.username = localStorage.getItem('username')
+        this.mainPlayer.skinColor = localStorage.getItem('skinColor')
 
-        // Create player standing animation
-        this.anims.create({
-            key: 'standing-front',
-            frames: this.anims.generateFrameNumbers('player-skin-' + skinColor, { start: 1, end: 1 })
-        })
-
-        // Play standing by default
-        this.player.play('standing-front')
-        this.player.visible = true
-
-        // Walking and standing animations in all directions
-        this.anims.create({
-            key: 'walking-front',
-            frames: this.anims.generateFrameNumbers('player-skin-' + skinColor, { start: 0, end: 2 }),
-            frameRate: 5,
-            repeat: -1,
-            yoyo: true
-        })
-
-        this.anims.create({
-            key: 'standing-back',
-            frames: this.anims.generateFrameNumbers('player-skin-' + skinColor, { start: 4, end: 4 })
-        })
-
-        this.anims.create({
-            key: 'walking-back',
-            frames: this.anims.generateFrameNumbers('player-skin-' + skinColor, { start: 3, end: 5 }),
-            frameRate: 5,
-            repeat: -1,
-            yoyo: true
-        })
-
-        this.anims.create({
-            key: 'standing-left',
-            frames: this.anims.generateFrameNumbers('player-skin-' + skinColor, { start: 7, end: 7 })
-        })
-
-        this.anims.create({
-            key: 'walking-left',
-            frames: this.anims.generateFrameNumbers('player-skin-' + skinColor, { start: 6, end: 8 }),
-            frameRate: 5,
-            repeat: -1,
-            yoyo: true
-        })
-
-        this.anims.create({
-            key: 'standing-right',
-            frames: this.anims.generateFrameNumbers('player-skin-' + skinColor, { start: 10, end: 10 })
-        })
-
-        this.anims.create({
-            key: 'walking-right',
-            frames: this.anims.generateFrameNumbers('player-skin-' + skinColor, { start: 9, end: 11 }),
-            frameRate: 5,
-            repeat: -1,
-            yoyo: true
-        })
-
-        // Player physics
-        this.physics.add.existing(this.player)
-        this.player.body.collideWorldBounds = true
-
-        // Follow the player
-        cam.startFollow(this.player)
-
-        // Pull in username
-        let username = localStorage.getItem('username')
+        // Draw main player character
+        this.createPlayerCharacter(this.mainPlayer)
+        this.setMainPlayerCharacter(this.mainPlayer)
 
         // Back button
-        let backButton = this.add.sprite(30, this.cameras.main.height - 30, 'button-back').setOrigin(0, 1).setScrollFactor(0)
+        let backButton = this.add.sprite(30, cam.height - 30, 'button-back').setOrigin(0, 1).setScrollFactor(0)
         backButton.setInteractive().on('pointerdown', () => {
             backButton.setFrame(2)
             this.goToCharacterSelect()
@@ -105,80 +43,188 @@ class Gameplay extends Phaser.Scene {
     }
 
     update () {
-
         // Move player
         this.input.keyboard.on('keydown-UP', () => {
-            this.player.body.velocity.y = -100
-
-            // Play walking animation
-            if (!this.player.anims.isPlaying){
-                this.player.play('walking-back')
-            }
+            this.movePlayer('UP', this.mainPlayer)
         })
 
         this.input.keyboard.on('keydown-DOWN', () => {
-            this.player.body.velocity.y = 100
-
-            // Play walking animation
-            if (!this.player.anims.isPlaying){
-                this.player.play('walking-front')
-            }
+            this.movePlayer('DOWN', this.mainPlayer)
         })
 
         this.input.keyboard.on('keydown-LEFT', () => {
-            this.player.body.velocity.x = -100
-
-            // Play walking animation
-            if (!this.player.anims.isPlaying){
-                this.player.play('walking-left')
-            }
+            this.movePlayer('LEFT', this.mainPlayer)
         })
 
         this.input.keyboard.on('keydown-RIGHT', () => {
-            this.player.body.velocity.x = 100
-
-            // Play walking animation
-            if (!this.player.anims.isPlaying){
-                this.player.play('walking-right')
-            }
+            this.movePlayer('RIGHT', this.mainPlayer)
         })
 
-        // Stop moving player
-
-
-        // Move player
+        // Stop player
         this.input.keyboard.on('keyup-UP', () => {
-            this.player.body.velocity.y = 0
-            this.player.play('standing-back')
+            this.stopPlayer('UP', this.mainPlayer)
         })
 
         this.input.keyboard.on('keyup-DOWN', () => {
-            this.player.body.velocity.y = 0
-            this.player.play('standing-front')
+            this.stopPlayer('DOWN', this.mainPlayer)
         })
 
         this.input.keyboard.on('keyup-LEFT', () => {
-            this.player.body.velocity.x = 0
-            this.player.play('standing-left')
+            this.stopPlayer('LEFT', this.mainPlayer)
         })
 
         this.input.keyboard.on('keyup-RIGHT', () => {
-            this.player.body.velocity.x = 0
-            this.player.play('standing-right')
+            this.stopPlayer('RIGHT', this.mainPlayer)
+        })
+    }
+
+    createPlayerCharacter (playerContainer) {
+
+        let player = this.add.sprite(0,0, 'player-skin-' + playerContainer.skinColor)
+        playerContainer.add(player);
+        player.visible = false
+
+        // Create player standing animation
+        this.anims.create({
+            key: 'standing-front-' + playerContainer.username,
+            frames: this.anims.generateFrameNumbers('player-skin-' + playerContainer.skinColor, { start: 1, end: 1 })
         })
 
+        // Play standing by default
+        player.play('standing-front-' + playerContainer.username)
+        player.visible = true
+
+        // Walking and standing animations in all directions
+        this.anims.create({
+            key: 'walking-front-' + playerContainer.username,
+            frames: this.anims.generateFrameNumbers('player-skin-' + playerContainer.skinColor, { start: 0, end: 2 }),
+            frameRate: 5,
+            repeat: -1,
+            yoyo: true
+        })
+
+        this.anims.create({
+            key: 'standing-back-' + playerContainer.username,
+            frames: this.anims.generateFrameNumbers('player-skin-' + playerContainer.skinColor, { start: 4, end: 4 })
+        })
+
+        this.anims.create({
+            key: 'walking-back-' + playerContainer.username,
+            frames: this.anims.generateFrameNumbers('player-skin-' + playerContainer.skinColor, { start: 3, end: 5 }),
+            frameRate: 5,
+            repeat: -1,
+            yoyo: true
+        })
+
+        this.anims.create({
+            key: 'standing-left-' + playerContainer.username,
+            frames: this.anims.generateFrameNumbers('player-skin-' + playerContainer.skinColor, { start: 7, end: 7 })
+        })
+
+        this.anims.create({
+            key: 'walking-left-' + playerContainer.username,
+            frames: this.anims.generateFrameNumbers('player-skin-' + playerContainer.skinColor, { start: 6, end: 8 }),
+            frameRate: 5,
+            repeat: -1,
+            yoyo: true
+        })
+
+        this.anims.create({
+            key: 'standing-right-' + playerContainer.username,
+            frames: this.anims.generateFrameNumbers('player-skin-' + playerContainer.skinColor, { start: 10, end: 10 })
+        })
+
+        this.anims.create({
+            key: 'walking-right-' + playerContainer.username,
+            frames: this.anims.generateFrameNumbers('player-skin-' + playerContainer.skinColor, { start: 9, end: 11 }),
+            frameRate: 5,
+            repeat: -1,
+            yoyo: true
+        })
+
+        // Player physics
+        this.physics.world.enable(playerContainer)
+        playerContainer.body.collideWorldBounds = true
+    }
+
+    setMainPlayerCharacter (playerContainer) {
+        // Follow the player
+        let cam = this.cameras.main
+        cam.startFollow(playerContainer)
+    }
+
+    movePlayer (direction, playerContainer) {
+        let player = playerContainer.list[0]
+
+        switch (direction) {
+            case 'UP':
+                playerContainer.body.velocity.y = -100
+
+                // Play walking animation
+                if (!player.anims.isPlaying){
+                    player.play('walking-back-' + playerContainer.username)
+                }
+            break
+
+            case 'DOWN':
+                playerContainer.body.velocity.y = 100
+
+                // Play walking animation
+                if (!player.anims.isPlaying){
+                    player.play('walking-front-' + playerContainer.username)
+                }
+            break
+
+            case 'LEFT':
+                playerContainer.body.velocity.x = -100
+
+                // Play walking animation
+                if (!player.anims.isPlaying){
+                    player.play('walking-left-' + playerContainer.username)
+                }
+            break
+
+            case 'RIGHT':
+                playerContainer.body.velocity.x = 100
+
+                // Play walking animation
+                if (!player.anims.isPlaying){
+                    player.play('walking-right-' + playerContainer.username)
+                }
+            break
+        }
+    }
+
+    stopPlayer (direction, playerContainer) {
+        let player = playerContainer.list[0]
+
+        switch (direction) {
+            case 'UP':
+                playerContainer.body.velocity.y = 0
+                player.play('standing-back-' + playerContainer.username)
+            break
+
+            case 'DOWN':
+                playerContainer.body.velocity.y = 0
+                player.play('standing-front-' + playerContainer.username)
+            break
+
+            case 'LEFT':
+                playerContainer.body.velocity.x = 0
+                player.play('standing-left-' + playerContainer.username)
+            break
+
+            case 'RIGHT':
+                playerContainer.body.velocity.x = 0
+                player.play('standing-right-' + playerContainer.username)
+            break
+        }
     }
 
     goToCharacterSelect () {
+        // Remove all animations
+        Object.entries(this.anims.anims.entries).forEach(([key]) => this.anims.remove(key))
         localStorage.removeItem('skinColor')
-        this.anims.remove('standing-front')
-        this.anims.remove('standing-back')
-        this.anims.remove('standing-left')
-        this.anims.remove('standing-right')
-        this.anims.remove('walking-front')
-        this.anims.remove('walking-back')
-        this.anims.remove('walking-left')
-        this.anims.remove('walking-right')
         this.scene.start('CharacterSelect')
     }
 
